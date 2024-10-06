f567ae14f8118241405e45bf0358c84e00c81321
ZunaedMamu <zunaied19988@gmail.com>
2024-10-06 13:42:12 +0600

    buttin work done

--- JS/test.js
+++ JS/test.js
@@ -1,9 +1,20 @@
-const isVerified ="";
+// const isVerified ="";
+// /*
 // if(isVerified === true){
 //     console.log("Verified")
 // }
 // else{
 //     console.log("Not Verified")
 // }
+// */
+// console.log(`${isVerified === true ?"Verified":"Not Verified"}`);
 
-console.log(`${isVerified === true ?"Verified":"Not Verified"}`);
+function getTime(time){
+    const hour = parseInt(time /3600);
+    const remainingSec = parseInt(time % 3600);
+    const min = parseInt(remainingSec / 60);
+    const sec = parseInt(remainingSec % 60)
+
+    return `${hour}:${min}:${sec}`;
+}
+console.log(getTime(14800))
--- JS/video.js
+++ JS/video.js
@@ -10,15 +10,26 @@ const loadCategory =()=>{
     .catch(err => console.log(err))
 
 }
+
+const loadCategoryVideos = (id)=>{
+//   alert(id);
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+  .then(res => res.json())
+  .then(data =>displayVideos(data.category))
+  .catch(err => console.log(err))
+}
+
 const displayCategory = (categories)=>{
     const categoryContainer = document.getElementById('category')
     categories.forEach((item) => {
         console.log(item);
         // create button
-        const button = document.createElement("button")
-        button.classList = 'btn';
-        button.innerText = item.category;
-        categoryContainer.append(button)
+        const buttonContainer = document.createElement("div");
+        buttonContainer.innerHTML=`
+        <button onclick="loadCategoryVideos(${item.category_id})" class="btn">${item.category}</button>
+        `
+        // button.innerText = item.category;
+        categoryContainer.append(buttonContainer)
     })
     // console.log(data);
 }
@@ -30,27 +41,50 @@ const loadVideo =()=>{
     .then(data => displayVideos(data.videos))
     .catch(err => console.log(err))
 }
-const cardDemo ={
-    category_id: "1001",
-    video_id: "aaab",
-    thumbnail: "https://i.ibb.co/QPNzYVy/moonlight.jpg",
-    title: "Midnight Serenade",
-    authors: [
-        {
-            profile_picture: "https://i.ibb.co/fDbPv7h/Noha.jpg",
-            profile_name: "Noah Walker",
-            verified: false
-        }
-    ],
-    others: {
-        views: "543K",
-        posted_date: ""
-    },
-    description: "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
+// const cardDemo ={
+//     category_id: "1001",
+//     video_id: "aaab",
+//     thumbnail: "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+//     title: "Midnight Serenade",
+//     authors: [
+//         {
+//             profile_picture: "https://i.ibb.co/fDbPv7h/Noha.jpg",
+//             profile_name: "Noah Walker",
+//             verified: false
+//         }
+//     ],
+//     others: {
+//         views: "543K",
+//         posted_date: ""
+//     },
+//     description: "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
+// }
+function getTime(time){
+    const hour = parseInt(time /3600);
+    const remainingSec = parseInt(time % 3600);
+    const min = parseInt(remainingSec / 60);
+    const sec = parseInt(remainingSec % 60)
+
+    return `${hour}:${min}:${sec}`;
 }
 
 const displayVideos =(videos) =>{
     const videoContainer = document.getElementById("video")
+    videoContainer.innerHTML ="";
+    if(videos.length === 0){
+        videoContainer.classList.remove("grid")
+        videoContainer.innerHTML =`
+        <div class="min-h-[300px] w-full flex flex-col justify-center items-center gap-5">
+        <img src="design/icon.png">
+        <h2 class="text-center text-2xl font-extrabold">No Content here</h2>
+        </div>
+        `;
+        return;
+    }
+    else{
+        videoContainer.classList.add("grid")
+    }
+    
     videos.forEach((video) => {
         console.log(video);
         // display part
@@ -61,7 +95,7 @@ const displayVideos =(videos) =>{
                 <img class="w-full h-full object-cover"
                 src=${video.thumbnail}
                 alt="Video" />
-                <span class="absolute bg-[#000000a8] text-gray-300 rounded-sm text-xs right-2 bottom-2 p-1">${"01:11"}</span>
+                ${video.others.posted_date?.length == 0? "": `<span class="absolute bg-[#000000a8] text-gray-300 rounded-sm text-xs right-2 bottom-2 p-1">${getTime(video.others.posted_date)}</span>`}
             </figure>
             
            
